fc829bc9707c265a9941650918d6f7147fc1fa6b
Neha Sachdev <nehasach@usc.edu>
2018-10-08 13:52:59 -0500

    Added Mongo DB

--- yelpcamp/v1/app.js
+++ yelpcamp/v1/app.js
@@ -1,6 +1,10 @@
 var express = require("express");
 var bodyParser=require("body-parser");
 var app=express();
+var mongoose=require("mongoose");
+
+mongoose.connect("mongodb://localhost/yelp_camp");
+
 app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({extended:true}));
 app.get("/", function(req,res){
@@ -8,44 +12,90 @@ app.get("/", function(req,res){
     res.render("landing");
 });
 
- var camp=[
-    {name:"Salmon Creek", image:"https://www.reserveamerica.com/webphotos/NH/pid270015/0/540x360.jpg"},
-    {name:"Golden Trail", image:"https://www.nps.gov/shen/planyourvisit/images/20170712_A7A9022_nl_Campsites_BMCG_960.jpg?maxwidth=1200&maxheight=1200&autorotate=false"},
-    {name:"Mountain Goats", image:"https://www.fs.usda.gov/Internet/FSE_MEDIA/stelprdb5306226.jpg"},
-    {name:"Salmon Creek", image:"https://www.reserveamerica.com/webphotos/NH/pid270015/0/540x360.jpg"},
-    {name:"Golden Trail", image:"https://www.nps.gov/shen/planyourvisit/images/20170712_A7A9022_nl_Campsites_BMCG_960.jpg?maxwidth=1200&maxheight=1200&autorotate=false"},
-    {name:"Mountain Goats", image:"https://www.fs.usda.gov/Internet/FSE_MEDIA/stelprdb5306226.jpg"},
-    {name:"Salmon Creek", image:"https://www.reserveamerica.com/webphotos/NH/pid270015/0/540x360.jpg"},
-    {name:"Golden Trail", image:"https://www.nps.gov/shen/planyourvisit/images/20170712_A7A9022_nl_Campsites_BMCG_960.jpg?maxwidth=1200&maxheight=1200&autorotate=false"},
-    {name:"Mountain Goats", image:"https://www.fs.usda.gov/Internet/FSE_MEDIA/stelprdb5306226.jpg"},
-    {name:"Salmon Creek", image:"https://www.reserveamerica.com/webphotos/NH/pid270015/0/540x360.jpg"},
-    {name:"Golden Trail", image:"https://www.nps.gov/shen/planyourvisit/images/20170712_A7A9022_nl_Campsites_BMCG_960.jpg?maxwidth=1200&maxheight=1200&autorotate=false"},
-    {name:"Mountain Goats", image:"https://www.fs.usda.gov/Internet/FSE_MEDIA/stelprdb5306226.jpg"},
-    {name:"Salmon Creek", image:"https://www.reserveamerica.com/webphotos/NH/pid270015/0/540x360.jpg"},
-    {name:"Golden Trail", image:"https://www.nps.gov/shen/planyourvisit/images/20170712_A7A9022_nl_Campsites_BMCG_960.jpg?maxwidth=1200&maxheight=1200&autorotate=false"},
-    {name:"Mountain Goats", image:"https://www.fs.usda.gov/Internet/FSE_MEDIA/stelprdb5306226.jpg"}
-    ];
-    
-app.get("/campgrounds", function(req,res){
-   res.render("campgrounds",{camp:camp})
+//SCHEMA SET UP
+
+var campgroundSchema=new mongoose.Schema({
+   name: String,
+   image: String,
+   description:String
 });
 
+var Campground=mongoose.model("Campground",campgroundSchema);
+
+
+//   Campground.create(
+//     {
+//   name:"Golden Trail",
+//   image:"https://img.sunset02.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/2016/10/main/hoodview-campground-0510.jpg?itok=xo0RuR6u",
+//   description:"This is my campground"
+// },function(error, campground){
+//     if(error){
+//     console.log(error);
+//     console.log("Error!!!");
+//     }
+//     else{
+//         console.log("Campground Created!");
+//         console.log(campground);
+       
+//     }
+// });
+//INDEX ROUTE    
+app.get("/campgrounds", function(req,res){
+    var camp=Campground.find({},function(err,camp){
+        if(err){
+            console.log(err);
+        }
+        else{
+            res.render("index",{camp:camp})
+        }
+    });
+  
+});
+//NEW ROUTE
 app.get("/campgrounds/new", function(req, res) {
   
     res.render("new.ejs");
 });
-
+//CREATE ROUTE
 app.post("/campgrounds", function(req,res) {
     // body...
     var name=req.body.name;
     var image=req.body.image;
+    var desc=req.body.desc;
     var newCamp={name: name, image:image}
-    camp.push(newCamp);
-    res.redirect("/campgrounds");
-    
+    Campground.create(
+    {
+  name:name,
+  image:image,
+  description:desc
+},function(error, campground){
+    if(error){
+    console.log(error);
+    console.log("Error!!!");
+    }
+    else{
+        console.log("Campground Created!");
+        console.log(campground);
+        res.redirect("/campgrounds");
+    }
+});
+   
+});
+//SHOW Route
+app.get("/campgrounds/:id",function(req, res) {
+   Campground.findById(req.params.id,function(err,foundCamp){
+       if(err){
+           console.log(err);
+       }
+       else
+       {
+         res.render("show",{camp:foundCamp});  
+       }
+   });
+   
+   
 });
 
-
 app.listen(process.env.PORT, process.env.IP, function(){
-    console.log("Yelp Camp Server has started!");
+    console.log("Yelp Camp Server has !");
 });
